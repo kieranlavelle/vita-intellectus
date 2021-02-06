472ec6f81c96937982f25b3cf99b7a8896e1c442
Kieran <kieran@clicktech.com>
2021-02-06 17:48:19 +0000

    Altered API to point at prod

--- UI/src/endpoints/http-config.js
+++ UI/src/endpoints/http-config.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    baseURL: `http://127.0.0.1:8004/`,
+    baseURL: `https://node404.com/gateway/vita`,
     headers: {
       "X-Authenticated-UserId": window.localStorage.getItem('username'),
+      "Authorization": "Bearer " + window.localStorage.getItem('token'),
       "Access-Control-Allow-Headers": "*"
     }
 })
