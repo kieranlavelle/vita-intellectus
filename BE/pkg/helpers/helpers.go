6556f60be07c410c70aad6c5add0ff105c0d5b49
kieran lavelle <kmplavelle@gmail.com>
2021-02-06 10:07:00 +0000

    Added helper functions for users and DB connection

--- BE/pkg/helpers/helpers.go
+++ BE/pkg/helpers/helpers.go
@@ -1,7 +1,13 @@
 package helpers
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
+
+	"github.com/kieranlavelle/vita-intellectus/pkg/users"
 )
 
 // ErrorExit sets the context and returns true when an error is found
@@ -11,4 +17,26 @@ func ErrorExit(err error, ctx *gin.Context, msg string, status int) bool {
 		return true
 	}
 	return false
+}
+
+// DatabaseConnection gets the connection out of the context
+func DatabaseConnection(c *gin.Context) (*pgx.Conn, error) {
+	conn, ok := c.MustGet("databaseConnection").(*pgx.Conn)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"detail": "Internal Server Error."})
+		return nil, errors.New("failed to get connection from context")
+	}
+
+	return conn, nil
+}
+
+// RequestUser gets the user out of the context
+func RequestUser(c *gin.Context) (users.User, error) {
+	user, ok := c.MustGet("user").(users.User)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"detail": "Internal Server Error."})
+		return users.User{}, errors.New("failed to get user from context")
+	}
+
+	return user, nil
 }
