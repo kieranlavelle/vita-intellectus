c996b8d5292e2330613a45a3c26f63b17874ba73
kieran lavelle <kmplavelle@gmail.com>
2021-02-09 10:38:36 +0000

    Added due dates structure to habbits.

--- BE/pkg/habbits/structs.go
+++ BE/pkg/habbits/structs.go
@@ -7,9 +7,16 @@ type Habbit struct {
 	Name           string   `json:"name"`
 	Days           []string `json:"days"`
 	CompletedToday bool     `json:"completed_today"`
+	NextDue        DueDates `json:"due_dates"`
 }
 
 // CompleteHabbitBody represents the body expected by the completeHabbit request
 type CompleteHabbitBody struct {
 	HabbitID int `json:"habbit_id"`
+}
+
+// DueDates represents when a habbit is next due depending on state
+type DueDates struct {
+	NextDue               string `json:"next_due"`
+	NextDueAfterCompleted string `json:"next_due_on_completed"`
 }
