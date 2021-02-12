a5c9a277816bdf173d7f4a0960a39702e11987c8
kieran lavelle <kmplavelle@gmail.com>
2021-02-12 19:16:56 +0000

    Added some quick helpers for login and signup nav

--- ui2/src/views/register.js
+++ ui2/src/views/register.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import { useHistory } from 'react-router-dom';
 
 import Card from '@material-ui/core/Card'
 import Grid from '@material-ui/core/Grid'
@@ -6,6 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import background from '../img/login-bg.jpg'
 import RegisterForm from '../components/forms/registerForm'
+import { CardContent } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -27,11 +30,19 @@ const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.up('lg')]: {
             width: '30%',
         },
+    },
+    text: {
+        color: 'black'
+    },
+    link: {
+        color: 'blue'
     }
   }));
 
 export default function Register() {
     const classes = useStyles()
+    const history = useHistory()
+    const redirect = useCallback((path) => history.push(path), [history]);
 
     return (
             <Grid
@@ -45,7 +56,12 @@ export default function Register() {
                     variant="elevation"
                     elevation={3}
                 >
-                    <RegisterForm />
+                    <CardContent>
+                        <RegisterForm />
+                        <p className={classes.text}>Registered? 
+                            <a className={classes.link} onClick={() => redirect('/login')}> Login!</a>
+                        </p>
+                    </CardContent>
                 </Card>
             </Grid>
     )
