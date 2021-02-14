07cb3d7c62e2978e1d61263b3a1df0ea789eec87
kieran lavelle <kmplavelle@gmail.com>
2021-02-14 16:20:46 +0000

    Updated base paths

--- ui2/src/routing/router.js
+++ ui2/src/routing/router.js
@@ -42,7 +42,7 @@ export default function Router(){
     const [token, setToken] = useStickyState("token", '');
 
     return (
-        <BrowserRouter basename={'/vita'}>
+        <BrowserRouter>
             <Switch>
                 <Route
                     exact
