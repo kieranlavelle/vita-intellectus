9879399632491904dbd37d5d913e62fbaef7ccee
kieran lavelle <kmplavelle@gmail.com>
2021-04-02 13:06:17 +0100

    Update Navbar.js
    Fixed the logout button.

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -5,8 +5,12 @@ import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import SettingsIcon from '@material-ui/icons/Settings';
-import Menu from '@material-ui/core/Menu';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
 import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
 
 
 import Button from '@material-ui/core/Button';
@@ -27,9 +31,8 @@ const useStyles = makeStyles((theme) => ({
 
 const SettingsMenu = () => {
 
-  const ITEM_HEIGHT = 48;
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
 
   const classes = useStyles();
   let history = useHistory();
@@ -41,43 +44,69 @@ const SettingsMenu = () => {
     };
   }, [token]);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  // return focus to the button when we transitioned from !open -> open
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    prevOpen.current = open;
+  }, [open]);
 
-  const logout = () => {
+  const logout = (event) => {
+    handleClose(event);
     setToken('');
   }
 
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+
   return (
-      <IconButton
-        className={classes.settings}
-        onClick={handleClick}
-        style={{padding: '0px'}}
-      >
-        <SettingsIcon />
-        <Menu
-          id="long-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={open}
-          onClose={handleClose}
-          PaperProps={{
-            style: {
-              maxHeight: ITEM_HEIGHT * 4.5,
-              width: '20ch',
-            },
-          }}
+        <IconButton
+          className={classes.settings}
+          style={{padding: '0px'}}
+          ref={anchorRef}
+          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
         >
-        <MenuItem onClick={logout}>
-          Logout
-        </MenuItem>
-      </Menu>
-      </IconButton>
+          <SettingsIcon />
+          <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                      <MenuItem onClick={logout}>Logout</MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </IconButton>
   )
 }
 
