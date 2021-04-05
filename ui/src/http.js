25ebd19d1a4aaca232ec8d996b217a3ba0ee76d4
kieran lavelle <kmplavelle@gmail.com>
2021-04-05 19:52:21 +0100

    Update http.js

--- ui/src/http.js
+++ ui/src/http.js
@@ -6,17 +6,17 @@ const AUTH = axios.create({
     headers: {}
 });
 
-// const API = axios.create({
-//     baseURL: 'https://gateway.node404.com/vita/',
-//     timeout: 1000,
-//     headers: {}
-// });
-
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8004/',
-    headers: {
-        'X-Authenticated-UserId': 'kmplavelle@gmail.com'
-    }
+    baseURL: 'https://gateway.node404.com/vita/',
+    timeout: 1000,
+    headers: {}
 });
+
+// const API = axios.create({
+//     baseURL: 'http://127.0.0.1:8004/',
+//     headers: {
+//         'X-Authenticated-UserId': 'kmplavelle@gmail.com'
+//     }
+// });
 
 export {API, AUTH}
